dff557a7c236818fb00c158e2961a217eb1097a6
leo6033 <long452a@163.com>
2020-11-11 05:20:32 +0800

    update(Texture.h): create class Texture

---
+++ RayTracing/src/scene/Texture.h
@@ -0,0 +1,52 @@
+/*
+ * @Description: 纹理相关类
+ * @Author: 妄想
+ * @Email: long452a@163.com
+ * @Date: 2020-11-10
+ */
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "../vecmath/DGM.h"
+
+namespace disc0ver {
+	class Texture {
+	public:
+		virtual rgb value(double u, double v, const point& p) const = 0;
+	};
+
+	class SolidColor : public Texture {
+	public:
+		SolidColor() = default;
+		SolidColor(rgb c) : color(c) {}
+		SolidColor(double r, double g, double b) : SolidColor(rgb(r, g, b)) {}
+
+		virtual rgb value(double u, double v, const point& p) const override {
+			return color;
+		}
+	private:
+		rgb color;
+	};
+
+	class checkerTexture : public Texture {
+	public:
+		checkerTexture() = default;
+
+		checkerTexture(std::shared_ptr<Texture> _even, std::shared_ptr<Texture> _odd) : even(_even), odd(_odd) {}
+
+		checkerTexture(rgb c1, rgb c2) : even(std::make_shared<SolidColor>(c1)), odd(std::make_shared<SolidColor>(c2)) {}
+
+		virtual rgb value(double u, double v, const point& p) const override {
+			auto sines = sin(10 * p[0]) * sin(10 * p[1]) * sin(10 * p[2]);
+			if (sines < 0)
+				return odd->value(u, v, p);
+			else
+				return even->value(u, v, p);
+		}
+	private:
+		std::shared_ptr<Texture> odd, even;
+	};
+}
+
+#endif // !TEXTURE_H
