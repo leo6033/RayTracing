6ea7d834110b6fba1190969a70fddcf9f27aeff6
leo6033 <long452a@163.com>
2020-11-17 12:40:33 +0800

    update: create class Trimesh

---
+++ RayTracing/src/sceneObjects/Trimesh.h
@@ -0,0 +1,24 @@
+/*
+ * @Description:
+ * @Author: НэПл
+ * @Email: long452a@163.com
+ * @Date: 2020-11-16
+ */
+
+#ifndef TRIMESH_H
+#define TRIMESH_H
+
+#include "../scene/Hitable.h"
+#include "Triangle.h"
+
+namespace disc0ver {
+
+	class Trimesh : public Hitable {
+	public:
+
+	public:
+		Vertex vertex;
+	};
+}
+
+#endif // !TRIMESH_H
